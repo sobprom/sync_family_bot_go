6832ef0fe88cac97d20635f573fddfc71f28ccc4
e.sobolev <evgenii.sobolev@stm-labs.ru>
2026-03-16 09:06:26 +0300

    поправил конфиг для ci/cd

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import "sync_family_bot_go/cmd"
 
-//go:generate jet -source=PostgreSQL -dsn=postgres://bot:bot@localhost:5433/bots_go?sslmode=disable -schema=family_sync -path=./internal/gen
-
 func main() {
 
 	// 1. Загружаем настройки
